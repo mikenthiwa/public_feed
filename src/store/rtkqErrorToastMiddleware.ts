998a08201a03a3e60d6a1ded78beb44b657f6cae
michael <mike.nthiwa@gmail.com>
2025-09-21 15:44:35 +0300

    Fix(unhandled error)

--- src/store/rtkqErrorToastMiddleware.ts
+++ src/store/rtkqErrorToastMiddleware.ts
@@ -2,7 +2,7 @@ import { isRejectedWithValue, ListenerMiddleware } from '@reduxjs/toolkit';
 import { toast } from 'react-toastify';
 
 export const rtkErrorToastMiddleware: ListenerMiddleware = () => (next) => (action) => {
-  if (isRejectedWithValue()) {
+  if (isRejectedWithValue(action)) {
     toast.error('Something went wrong');
   }
   return next(action);
