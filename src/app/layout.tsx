8c5ca9ed235da020f442fea247f9ad61ce50ed5f
michael <mike.nthiwa@gmail.com>
2025-10-07 17:20:17 +0300

    chore(theme-test)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
+    <html lang='en' className='light-theme'>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <AppRouterCacheProvider options={{ enableCssLayer: true }}>
           <CssBaseline />
